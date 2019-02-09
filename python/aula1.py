fb4741a5eb9c21b4cae9550b233dcad845f29a4c
Gabriela Gonçalves <gabi_goncalves99@hotmail.com>
2019-02-08 22:40:16 -0200

    introdução Python 3

---
+++ python/aula1.py
@@ -0,0 +1,33 @@
+# comentario em apenas uma linha
+
+""" tres aspas simples ou duplas
+para comentario em bloco """
+
+#criando variavel:
+
+nome = "Gabriela"
+
+#imprimindo mensagem:
+#print é uma função que aceita varios parametros como + e ,
+
+print("Olá, " + nome)
+
+#entrada de dados pelo terminal, por padro toda entrada no terminal é string:
+
+nome = input("Digite seu nome: ")
+print(nome + "bem vindo ao sistema!")
+
+#quando trabalhamos com numeros precisamos converter os valores das variaveis
+
+idade = input("Digite sua idade: ")
+idade = int(idade)
+
+#idade = float(idade)
+
+""" operadores lógicos: +, -, * e / são iguais no java
+    para calcular potencia usa **, exemplo: 2**3
+     and e ou
+
+    condicionais:
+"""
+    a = 20
